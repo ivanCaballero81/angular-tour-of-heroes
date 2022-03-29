abfddc39873f9352920e26d7d8ff206a1d938aa4
Ivan Caballero <ivan.caballero@adesso.de>
2022-03-29 12:39:48 +0200

    add Routing and css-Styling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import {HeroService} from "../hero.service";
-import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,18 +10,13 @@ import {MessageService} from "../message.service";
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(private heroService: HeroService,private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
